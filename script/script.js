4c8efc47294e42bc1bfefb2151f56faf8d41220c
Tatyana <99961462+TatyanaDaviduk@users.noreply.github.com>
2023-12-07 11:28:54 +0300

    add slider

--- script/script.js
+++ script/script.js
@@ -26,3 +26,32 @@ accordionItem.forEach((item) => {
         item.classList.toggle('accordion__item--show');
     })
 })
+
+
+const swiper = new Swiper('.mySlider', {
+    effect: "coverflow",   
+    grabCursor: true,  
+    centerSlides: true,
+    slidesPerView: 1.5,
+    coverflowEffect: {
+        slideShadows: false,
+        depth: 100,
+        rotate:-2,
+        modifier: -1,
+        stretch: "-92%"
+    },
+      // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true, //включаем поддержку динамических буллетов, если не включить отобр все точки
+        dynamicMainBullets: 2, //максимальное количество отображаемых буллетов
+    },
+    navigation: {
+        nextEl: '.mySlider__button-next',
+        prevEl: '.mySlider__button-prev',
+      },
+
+
+  
+  });
